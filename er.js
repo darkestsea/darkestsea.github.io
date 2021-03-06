8fc58e31ba412fc4e30bfb33220b85dcbeced18f
darkestsea <76624937+darkestsea@users.noreply.github.com>
2021-03-06 17:36:26 +0200

    Add files via upload

---
+++ er.js
@@ -0,0 +1,7 @@
+//localStorage.setItem("isim","mehmet");
+//localStorage.clear();
+//alert(localStorage.getItem('isim'));
+function e(){
+	tex=document.getElementById("eren").value;
+	localStorage.setItem("deger",tex)
+}
